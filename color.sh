dfe5a83c267fdcf8228be13ae7d96b8cbf2b22f2
BinbinLee <lbbxsxlz@gmail.com>
2021-01-01 20:57:23 +0800

    add shell color

---
+++ color.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+printf -- ' this script testing color... \n'
+printf -- '\033[37m lbbxsxlz testing output: \033[0m\n'
+printf -- '\033[32m SUCCESS: Green \033[0m\n'
+printf -- '\033[33m WARNING: Yellow \033[0m\n'
+printf -- '\033[31m ERROR: Red \033[0m\n'
