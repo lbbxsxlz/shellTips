545a50725c8f036ced88381c47de2b88831364c5
BinbinLee <lbbxsxlz@gmail.com>
2021-10-19 16:19:45 +0800

    add dd cmd skip param

--- ddCmd.sh
+++ ddCmd.sh
@@ -3,3 +3,6 @@
 dd if=/dev/zero bs=2k count=2 | tr '/000' '/3777' > tmp.txt
 dd if=tmp1.txt of=tmp.txt bs=2k count=1 conv=notrunc seek=0
 dd if=tmp2.txt of=tmp.txt bs=2k count=1 conv=notrunc seek=1
+
+# 从emmc中跳过1396M，然后读取1536M数据
+dd if=/dev/mmcblk0 of=firmware1 bs=1M skip=1396 count=1536
