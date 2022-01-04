7bfe738ad2d2b890fe5c3bf30d5aff6a209b9103
BinbinLee <lbbxsxlz@gmail.com>
2022-01-04 18:01:48 +0800

    update unix2dos cmd

--- dos2unix.sh
+++ dos2unix.sh
@@ -2,5 +2,12 @@
 cp tmpfile1 tmpfile2
 dos2unix tmpfile1
 diff tmpfile1 tmpfile2
+
 cat tmpfile2 | tr -d '\r' > tmpfile3
 diff tmpfile3 tmpfile2
+
+dos2unix -n tmpfile2 tmpfile4
+diff tmpfile4 tmpfile2
+
+unix2dos-n tmpfile4 tmpfile5
+diff tmpfile4 tmpfile2
