9250a651e0711be889b51e313ecbcc03f27d0c2d
BinbinLee <lbbxsxlz@gmail.com>
2022-07-13 14:36:10 +0800

    update initrd format

--- mkinitrd.sh
+++ mkinitrd.sh
@@ -5,4 +5,6 @@ gunzip -f initrd.gz
 cpio -ivmd < initrd
 rm initrd
 # add or replace files
-find . | cpio -c -o -v | gzip -9 -n >../initrd
+# find . | cpio -c -o -v | gzip -9 -n >../initrd
+#find . | cpio -H newc -o -v | gzip -9 >../initrd
+find . | cpio -H newc --create --verbose | gzip -9 >../initrd
