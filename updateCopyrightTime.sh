39b2e2e0261638213379a67ea0c528084621f450
BinbinLee <lbbxsxlz@gmail.com>
2023-01-05 18:57:49 +0800

    update

--- updateCopyrightTime.sh
+++ updateCopyrightTime.sh
@@ -1,7 +1,16 @@
-#! /bin/bash
+#! /bin/sh
+# author lbbxsxlz@gmail.com
 
 filelist=`git status | grep modified | awk -F " " '{print $2}'`
 for file in $filelist
 do
-	sed '/Copyright/ s/[0-9]\{4\}/'$(date '+%Y')'/2' -i $file
+	count=`cat $file | grep Copyright | grep -o '[0-9]\{4\}' | wc -l`
+	if [ $count -eq 1 ]; then
+		oldyear=`cat $file | grep Copyright | grep -o '[0-9]\{4\}'`
+		sed "/Copyright/ s/[0-9]\{4\}/$oldyear-$(date '+%Y')/" -i $file
+		#sed '/Copyright/ s/[0-9]\{4\}/'"$oldyear"'-'$(date '+%Y')'/' -i $file
+	else
+		sed "/Copyright/ s/[0-9]\{4\}/$(date '+%Y')/2" -i $file
+		#sed '/Copyright/ s/[0-9]\{4\}/'$(date '+%Y')'/2' -i $file
+	fi
 done
