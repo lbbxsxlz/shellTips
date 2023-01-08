a3b0bf87ae609a7c755e1e794155c3fb915cbb2e
BinbinLee <lbbxsxlz@gmail.com>
2023-01-08 21:35:21 +0800

    Update awkCmd.sh

--- awkCmd.sh
+++ awkCmd.sh
@@ -4,3 +4,8 @@ cat tmp.txt | grep lbbxsxlz | awk -F '.' '{print $1}'
 
 cat tmp.txt | grep lbb | awk -F ':' 'NR==1 {print $1}'
 
+awk 'BEGIN{a=5;b=3;print "a * b =",a*b}'
+
+awk 'BEGIN{print ENVIRON["HOME"]}'
+
+awk -F: '$NF=="/bin/bash"{printf "%15s,%s\n",$NF,$1}' /etc/passwd
