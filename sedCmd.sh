0a8c74d530bb954997432c8fc2d5aaf082138202
BinbinLee <lbbxsxlz@gmail.com>
2023-01-08 21:42:20 +0800

    Update sedCmd.sh

--- sedCmd.sh
+++ sedCmd.sh
@@ -4,3 +4,6 @@ sed -i '/lbbxsxlz/d' tmp.txt
 sed -i '/sls/a lbbxsxlzlovesls' tmp.txt
 sed -e "/sls/i \\\t\t\"lbbxsxlzloveslslove\"" tmp.txt > tmp.bak
  
+sed -n '/^root/,/^sync/p' /etc/passwd
+sed -n '5, +5p' /etc/passwd
+sed '/^#/d' /etc/crontab
