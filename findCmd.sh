0adfb1171d60fd6ba656b16885128ae705f324c0
BinbinLee <lbbxsxlz@gmail.com>
2023-01-15 22:56:11 +0800

    update command demos

--- findCmd.sh
+++ findCmd.sh
@@ -2,6 +2,8 @@
 
 find ~ -name "*.txt" -print
 
+find ./ -type f -print 
+
 find /usr/bin -size +10000k -exec ls -ld {} \;
 
 find . -perm 777 -print
--- sedCmd.sh
+++ sedCmd.sh
@@ -7,3 +7,6 @@ sed -e "/sls/i \\\t\t\"lbbxsxlzloveslslove\"" tmp.txt > tmp.bak
 sed -n '/^root/,/^sync/p' /etc/passwd
 sed -n '5, +5p' /etc/passwd
 sed '/^#/d' /etc/crontab
+
+sed -i 's/\x00\x00\x38\x38\x38\x38\x38\x38\x00/\x00\x00\x38\x38\x38\x38\x38\x33\x00/' ./sonia_upx_bak
+sed -i '/"Commands"/a\\      "burn dhboot.bin.img bootloader",' ./Install
