94f23ec1e4d450d3519f8bf18cf3175e9c05ac3b
BinbinLee <lbbxsxlz@gmail.com>
2023-02-10 11:35:59 +0800

    update 7z command

--- 7zCmd.sh
+++ 7zCmd.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+# show the format supported
 7z i
+
+# show the the format of file
+file $filename
+
+# list contens
 7z l hello.ext4
+
+# extract files
 7z x hello.ext4
+# -snl store symbolic links as links
+7z -snl hello.ext4
