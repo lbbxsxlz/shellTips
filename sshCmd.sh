e4d1689ebf226b7d2053341482c97175ef1ba031
BinbinLee <lbbxsxlz@gmail.com>
2023-03-25 18:56:12 +0800

    add ssh-keygen operation
    Signed-off-by: BinbinLee <lbbxsxlz@gmail.com>

--- sshCmd.sh
+++ sshCmd.sh
@@ -19,3 +19,10 @@ ssh -C -f -N -g -L 8321:192.168.254.201:4321 admin@192.168.254.201
 # 动态端口转发
 ssh -f -N -D  localhost:9966 admin@172.31.3.129
 
+# fix error "WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!"
+# 140.82.121.4 是报错的IP
+ssh-keygen -R 140.82.121.4
+
+# 生成rsa密钥
+ssh-keygen -t rsa
+
