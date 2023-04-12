e98ebba5f5fc5e1feaa245550983cd38ddc18ed3
BinbinLee <lbbxsxlz@gmail.com>
2023-04-12 14:34:06 +0800

    update sed command

--- sedCmd.sh
+++ sedCmd.sh
@@ -8,5 +8,12 @@ sed -n '/^root/,/^sync/p' /etc/passwd
 sed -n '5, +5p' /etc/passwd
 sed '/^#/d' /etc/crontab
 
+# remove the space
+sed 's/[[:space:]]//g' -i tmp
+sed 's/-//g' -i tmp
+
+# remove '\n'
+sed ":a;N;s/\n//g;ta" -i tmp
+
 sed -i 's/\x00\x00\x38\x38\x38\x38\x38\x38\x00/\x00\x00\x38\x38\x38\x38\x38\x33\x00/' ./sonia_upx_bak
 sed -i '/"Commands"/a\\      "burn dhboot.bin.img bootloader",' ./Install
