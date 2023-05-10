7bc6088c288e4fd94e0c339654924011c5548bd0
BinbinLee <lbbxsxlz@gmail.com>
2023-05-10 15:42:09 +0800

    Create array.sh

---
+++ array.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+declare -A user
+
+user[name]='lbbxsxlz'
+user[age]=34
+
+echo ${user[name]}
+
+echo ${user[*]}
+
+funclist=(func1 func2 func3)
+
+func1()
+{
+        echo 'this is a function named func1'
+}
+
+
+func2()
+{
+        echo 'this a another function'
+}
+
+func3()
+{
+        echo 'this a another function name func3'
+}
+
+
+
+for f in ${funclist[*]}
+do
+        echo $f
+        $f
+done
