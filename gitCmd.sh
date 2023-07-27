8a33310c6dd9c6bb52bfd07b46f506fb7787afd4
BinbinLee <lbbxsxlz@gmail.com>
2023-07-27 15:32:49 +0800

    Update gitCmd.sh

--- gitCmd.sh
+++ gitCmd.sh
@@ -20,6 +20,7 @@ git reset --hard HEAD/$commitID
 git clone -b 2.3.1 https://github.com/DMTF/libspdm.git
 
 git diff > code.patch
+git apply --check code.patch
 git apply code.patch
 
 git commit -m "commit message"
