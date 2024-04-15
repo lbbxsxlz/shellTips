04b63a8f9b531c0cec988537c2e758413cbf7348
BinbinLee <lbbxsxlz@gmail.com>
2024-04-15 14:07:42 +0800

    add new example

--- awkCmd.sh
+++ awkCmd.sh
@@ -9,3 +9,5 @@ awk 'BEGIN{a=5;b=3;print "a * b =",a*b}'
 awk 'BEGIN{print ENVIRON["HOME"]}'
 
 awk -F: '$NF=="/bin/bash"{printf "%15s,%s\n",$NF,$1}' /etc/passwd
+
+tmp=9;awk -v av="$tmp" '{print $av}' file.txt
